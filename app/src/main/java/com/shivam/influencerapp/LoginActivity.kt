9dad2b43673b40578dc470dd7954bcd8971fa159
shivamsoods <shivam.sood708@gmail.com>
2020-03-28 13:21:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/shivam/influencerapp/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.shivam.influencerapp
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        supportActionBar?.hide()
+
+    }
+}
