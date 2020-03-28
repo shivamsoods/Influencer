52f465aef476f522e9df4ffb63640de6f5327dee
shivamsoods <shivam.sood708@gmail.com>
2020-03-28 17:34:05 +0530

    Finished Login

--- app/src/main/java/com/shivam/influencerapp/LoginActivity.kt
+++ app/src/main/java/com/shivam/influencerapp/LoginActivity.kt
@@ -1,9 +1,13 @@
 package com.shivam.influencerapp
 
+
 import android.os.Bundle
-import android.view.Window
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -12,13 +16,86 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val w: Window = window
-        w.setFlags(
+        hideActionBar()
+
+        val topCurveAnim =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.top_down)
+
+
+        val editTextAnim =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.edit_text_anim)
+
+
+        val textViewAnim =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.edit_text_anim)
+        val centerRevealAnim =
+            AnimationUtils.loadAnimation(applicationContext, R.anim.center_reveal_anim)
+
+
+        btnLoginEmail.setOnClickListener {
+            grpLoginButton.visibility = View.GONE
+            cvLoginMobile.visibility = View.GONE
+            cvLoginEmail.visibility = View.VISIBLE
+
+            etLoginEmail.startAnimation(editTextAnim)
+            etLoginPassword.startAnimation(editTextAnim)
+
+            tvLoginEmail.startAnimation(textViewAnim)
+            tvLoginPassword.startAnimation(textViewAnim)
+        }
+
+        tvLoginSwitchToEmail.setOnClickListener {
+            grpLoginButton.visibility = View.GONE
+            cvLoginMobile.visibility = View.GONE
+            cvLoginEmail.visibility = View.VISIBLE
+
+            etLoginEmail.startAnimation(editTextAnim)
+            etLoginPassword.startAnimation(editTextAnim)
+
+            tvLoginEmail.startAnimation(textViewAnim)
+            tvLoginPassword.startAnimation(textViewAnim)
+        }
+
+        tvLoginSwitchToMobile.setOnClickListener {
+            grpLoginButton.visibility = View.GONE
+            cvLoginMobile.visibility = View.VISIBLE
+            cvLoginEmail.visibility = View.GONE
+
+            etLoginMobile.startAnimation(editTextAnim)
+            tvLoginMobile.startAnimation(textViewAnim)
+        }
+
+
+        btnLoginMobile.setOnClickListener {
+            grpLoginButton.visibility = View.GONE
+            cvLoginMobile.visibility = View.VISIBLE
+            cvLoginEmail.visibility = View.GONE
+
+            etLoginMobile.startAnimation(editTextAnim)
+            tvLoginMobile.startAnimation(textViewAnim)
+        }
+
+        btnLoginEmail2.setOnClickListener {
+            Toast.makeText(applicationContext, "Email used", Toast.LENGTH_SHORT).show()
+        }
+
+        btnLoginMobile2.setOnClickListener {
+            Toast.makeText(applicationContext, "Mobile used", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+        ivLoginTopShape.startAnimation(topCurveAnim)
+
+
+    }
+
+    private fun hideActionBar() {
+        window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
         supportActionBar?.hide()
-
     }
 }
